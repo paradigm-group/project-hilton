0941c2348fee4ca0e903e15ce6020ac48ef1297a
Jack Machin <jack.machin125@gmail.com>
2016-04-07 16:29:16 +0100

    Homepage

--- page-home.php
+++ page-home.php
@@ -21,67 +21,52 @@
 
             <div class="main"  role="main">
 
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="newsticker cf">
+                    <?php $loop = new WP_Query( array( 'post_type' => 'group-news', 'posts_per_page' => 2 ) ); ?>
+                    <?php if($loop->have_posts()): ?>
+                    <ul class="home-ticker">
+                        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                            <li><a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="date"><?php the_date(); ?></span><?php the_excerpt(); ?></li>
+                        <?php endwhile; ?>
+                    </ul>
+                    <?php endif; ?>
+                </div>
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="portal">
+                    <?php wp_nav_menu(array(
+                        'container' => 'div',                           // contain the menu in a div
+                        'container_class' => 'home-menu-container cf',       // class of container (should you choose to use it)
+                        'menu' => __( 'Home Portal', 'bonestheme' ),  // nav name
+                        'menu_class' => 'nav home-nav cf',               // adding custom nav class
+                        'theme_location' => 'home-portal',                 // where it's located in the theme
+                        'before' => '',                                 // before the menu
+                        'after' => '',                                  // after the menu
+                        'link_before' => '',                            // before each link
+                        'link_after' => '',                             // after each link
+                        'depth' => 0,                                   // limit the depth of the nav
+                        'fallback_cb' => ''                             // fallback function (if there is one)
+                    )); ?>
+                </div>
 
-                    <header class="article-header">
+                <div class="beat-container">
+                    <a href="/pulse">
+                        <img src="<?php echo get_template_directory_uri();?>/images/pulse-1.png" width="250" class="aligncenter">
+                    </a>
+                    <div class="beat">
+                        <?php $loop = new WP_Query( array( 'post_type' => 'pulse', 'posts_per_page' => 3 ) ); ?>
+                        <?php if($loop->have_posts()): ?>
+                            <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                                <h2 class="beat-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="beat-date"><?php the_date(); ?></span></h2>
+                                <div class="beat-excerpt"><?php the_excerpt(); ?></div>
+                            <?php endwhile; ?>
 
-                        <h1 class="page-title"><?php the_title(); ?></h1>
-
-      <?php get_template_part ('partials/post-meta' , 'page');?>
-
-                    </header>
-
-                    <div class="entry-content cf" itemprop="articleBody">
-                        <?php
-                            // the content (pretty self explanatory huh)
-                            the_content();
-
-                            /*
-                             * Link Pages is used in case you have posts that are set to break into
-                             * multiple pages. You can remove this if you don't plan on doing that.
-                             *
-                             * Also, breaking content up into multiple pages is a horrible experience,
-                             * so don't do it. While there are SOME edge cases where this is useful, it's
-                             * mostly used for people to get more ad views. It's up to you but if you want
-                             * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-                             *
-                             * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-                             *
-                            */
-                            wp_link_pages( array(
-                                'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-                                'after'       => '</div>',
-                                'link_before' => '<span>',
-                                'link_after'  => '</span>',
-                            ) );
-                        ?>
+                        <?php else : ?>
+                            <h2 class="beat-title soon">Coming soon!</h2>
+                        <?php endif; ?>
                     </div>
-
-
-                    <footer class="article-footer">
-
-      <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-                    </footer>
-
-                    <?php comments_template(); ?>
-
-                </article>
-
-                <?php endwhile; else : ?>
-
-    <?php get_template_part ('partials/no-post-found');?>
-
-                <?php endif; ?>
-
+                </div>
             </div>
-
-            <?php get_sidebar(); ?>
-
+        </div>
     </div>
-
-</div>
 
 <?php get_footer(); ?>
