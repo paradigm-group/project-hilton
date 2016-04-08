7083c9924c6052e7ed3ed4553816553674843e53
Jack Machin <jack.machin125@gmail.com>
2016-04-08 10:27:55 +0100

    The Enemey

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
             <div id="inner-header" class="wrapper">
                 <div class="logo-container">
                     <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-                    <h1 id="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
+                    <h1 class="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
 
                     <?php // if you'd like to use the site description you can un-comment it below ?>
                     <?php // bloginfo('description'); ?>
--- library/bones.php
+++ library/bones.php
@@ -138,7 +138,7 @@ function bones_scripts_and_styles() {
 		// ie-only style sheet
 		wp_register_style( 'guybrush-ie-only', get_stylesheet_directory_uri() . '/library/css/ie.css', array(), '' );
 
-        wp_register_style('google-fonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+        wp_register_style('google-fonts', 'http://fonts.googleapis.com/css?family=Alfa+Slab+One');
 
 		//adding scripts file in the footer
 		wp_register_script( 'guybrush-js', get_stylesheet_directory_uri() . '/library/js/min/scripts.min.js', array( 'jquery' ), '', true );
